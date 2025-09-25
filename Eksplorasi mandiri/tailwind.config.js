3d2d762d3db4e74cf6b3ce11de0e9f5ae01cc808
Nadya <nadyatirta.page@gmail.com>
2025-09-25 18:36:02 +0700

    up eksplorasi mandiri
    project frresh dengan dark mode toggle button

---
+++ Eksplorasi mandiri/tailwind.config.js
@@ -0,0 +1,18 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class',
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [forms],
+}
