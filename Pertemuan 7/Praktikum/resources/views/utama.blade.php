6dcc37952aab4cf1d8a7848e3948483c3188f7e7
Nadya <nadyatirta.page@gmail.com>
2025-10-10 08:07:46 +0700

    up project pert 7
    up project pert 7 sebelum kode dari praktikum

---
+++ Pertemuan 7/Praktikum/resources/views/utama.blade.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="{{ asset(path: 'css/style.css') }}">
+        <!--<style>
+            background-color: blue;
+        </style>-->
+    </head>
+    <body>
+        <nav>
+            <p>Ini Adalah Menu Navigasi bar</p>
+        </nav>
+        @yield(section: 'konten')
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
